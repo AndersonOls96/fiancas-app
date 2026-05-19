e77b3c2aa917678e39067be1798715a347d422cc
Anderson Oliveira Da Silva <11052@senacminas.edu.br>
2026-05-19 15:22:06 -0300

    Component ballance

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,6 @@ export default function Header({ name }) {
             </View>
         </View>
     )
-
 }
 
 const styles = StyleSheet.create({
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import Header from "../../components/Header";
+import Balance from "../../components/Balance";
 
 export default function Home(){
     return(
         <View style={styles.container}>
             <Header name="João Silva"/>
+            <Balance saldo="230,00" gastos="-200,00"/>
         </View>
     )
 }
